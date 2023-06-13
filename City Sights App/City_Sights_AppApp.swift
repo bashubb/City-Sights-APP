27e0244339a0ecd04a71e6e285328bb78122148e
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-06-13 11:32:29 +0200

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by HubertMac on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
