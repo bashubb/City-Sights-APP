d7f8bdb4e8df1b8ee3116d389e5deb52c6fc6c2c
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-06-13 21:45:14 +0200

    - add "getBusinesses" method to the ContentModel - it creates url, and makes url request to the API

--- City Sights App/ViewModels/ContentModel.swift
+++ City Sights App/ViewModels/ContentModel.swift
@@ -44,11 +44,60 @@ class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         //Gives us the lopcation of the user
-        print(locations.first ?? "no location")
+        let userLocation = locations.first
         
-        //Stop requesting the location after we get it once
-        locationManager.stopUpdatingLocation()
+        if userLocation != nil {
+            
+            // We have a location
+            //Stop requesting the location after we get it once
+            locationManager.stopUpdatingLocation()
+            
+            // If we have the coordinates of the user, send into Yelp API
+            //getBusinesses(category: "arts", location: userLocation!)
+            getBusinesses(category: "restaurants", location: userLocation!)
+        }
+    }
+    
+    // MARK: - Yelp API methods
+    
+    func getBusinesses(category:String, location:CLLocation) {
+    
+        // Create URL
+        /*
+        let urlString = "https://api.yelp.com/v3/businesses/search?latitude=\(location.coordinate.latitude)&longitude=\(location.coordinate.longitude)&categories=\(category)&limit=6"
         
-        // TODO : IF we have the coordinates of the user, send into Yelp API
+        let url = URL(string: urlString)
+         */
+        var urlComponents = URLComponents(string: "https://api.yelp.com/v3/businesses/search")
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "latitude", value: String(location.coordinate.latitude)),
+            URLQueryItem(name: "longitude", value: String(location.coordinate.longitude)),
+            URLQueryItem(name: "categories", value: category),
+            URLQueryItem(name: "limit", value: "6"),
+        ]
+        var url = urlComponents?.url
+        
+        if let url = url {
+            
+            
+            //Create URL Request
+            var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+            request.httpMethod = "GET"
+            request.addValue("Bearer AJoDFpWkhzu2tU5XdJAFtTzgZ6Op3WUPGyFx2hYixB97L99_5JlZZQJeNOwXrvtovFDppKxjIpBKI5iBGb324WPp6AsP3SYUjA972zMQUeNV5CLy-u_BGB0Y2rGIZHYx", forHTTPHeaderField: "Authorization")
+            
+            // Get URLSession
+            let session = URLSession.shared
+            
+            // Create Data task
+            let dataTask = session.dataTask(with: request) { data, response, error in
+                //Check that there isn't an error
+                if error == nil {
+                    print(response ?? "")
+                }
+            }
+            
+            // Start the Data Task
+            dataTask.resume()
+        }
     }
 }
